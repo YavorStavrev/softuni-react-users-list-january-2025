a1cc64a287b7b60d1834c29e5d5efbf4f823ef0a
YavorStavrev <156719018+YavorStavrev@users.noreply.github.com>
2025-03-16 16:35:54 +0200

    Fix post data

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -4,17 +4,23 @@ export default {
     async getAll() {
         const response = await fetch(baseUrl);
         const result = await response.json();
-        const users = Object.values(result); 
+        const users = Object.values(result);
 
         return users;
     },
-    async create(userData){
+    async create(userData) {
+        const { country, city, street, streetNumber, postData } = userData;
+
+        postData.address = { country, city, street, streetNumber };
+        postData.createdAt = new Date().toISOString();
+        postData.updatedAt = new Date().toISOString();
+
         const response = await fetch(baseUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(userData),
+            body: JSON.stringify(postData),
         });
 
         const result = await response.json();
