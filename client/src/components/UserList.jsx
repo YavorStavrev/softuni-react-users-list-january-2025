866d14de676a36d617ffd11a86d80de81049028d
YavorStavrev <156719018+YavorStavrev@users.noreply.github.com>
2025-03-16 15:43:41 +0200

    Show user create modal on add user click

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -3,9 +3,11 @@ import Pagination from "./Pagination";
 import Search from "./Search";
 import UserListItem from "./UserListItem";
 import userService from "../services/userService";
+import UserCreate from "./UserCreate";
 
 export default function UserList() {
     const [users, setUsers] = useState([]);
+    const [showCreate, setShowCreate] = useState(false);
 
     useEffect(() => {
         userService.getAll()
@@ -14,11 +16,15 @@ export default function UserList() {
             })
     }, []);
 
+    const addUserClickHandler = () => {
+        setShowCreate(true);
+    }
+
     return (
         <section className="card users-container">
 
             <Search />
-
+            {showCreate && <UserCreate />}
             {/* <!-- Table component --> */}
             <div className="table-wrapper">
                 <div className="overlays">
@@ -141,7 +147,7 @@ export default function UserList() {
             </div>
 
             {/* <!-- New user button  --> */}
-            <button className="btn-add btn">Add new user</button>
+            <button className="btn-add btn" onClick={addUserClickHandler}>Add new user</button>
 
 
             <Pagination />
